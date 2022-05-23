bbab203b687fd7f6a9813b48a5b264448cf66965
YoussefAdelElsayed <youssefadelelsayed30@gmail.com>
2022-05-23 17:19:24 +0200

    add job views & urls

--- job/models.py
+++ job/models.py
@@ -1,5 +1,6 @@
 from email.policy import default
 from pydoc import describe
+from unicodedata import name
 from django.db import models
 
 
@@ -20,10 +21,19 @@ class job (models.Model): #table
     vacance = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     exeperince = models.IntegerField(default=1)
-
+    category = models.ForeignKey('category', on_delete = models.CASCADE ,)
 
 
 
 
     def __str__(self):
         return self.title
+
+
+
+
+class category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
